7bf3edaa7daa5729db5da2a46d37b09c6b51fd0f
emedbo <emedbo@gmail.com>
2015-03-06 14:03:53 +0100

    endret mailservice

--- public/api/utils/agenda.js
+++ public/api/utils/agenda.js
@@ -1,15 +1,15 @@
 var agenda = require('agenda')({db: {address: process.env.CUSTOMCONNSTR_MONGOLAB_URI ||  'localhost/hks'}});
 var nodemailer = require('nodemailer');
 var Participant = require('../models/participant.server.model');
-var sendGridUser = process.env.SENDGRID_USER;
-var sendGridPassword = process.env.SENDGRID_PASS;
+var sendGridUser = process.env.MAILSERVICE_USER;
+var sendGridPassword = process.env.MAILSERVICE_PASS;
 
 agenda.define('join email', function (job, done) {
 
     Participant.findById(job.attrs.data.participantId).exec(function (err, participant) {
 
         var smtpTransport = nodemailer.createTransport({
-            service: 'SendGrid',
+            service: 'Mailgun',
             auth: {user: sendGridUser, pass: sendGridPassword}
         });
 
